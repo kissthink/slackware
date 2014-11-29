10f021f66a8e420e326ba536d32f6df6bf07ad26
Niki Kovacs <info@microlinux.fr>
2014-11-29 20:29:09 +0100

    ...

---
+++ testing/depcheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# depcheck.sh
+#
+# This script is a very basic dependency checker for Slackware Linux. It scans
+# all the binaries in the PATH and reports any missing libraries.
+
+for DIRECTORY in $(echo $PATH | sed 's/:/ /g'); do
+  for FILE in $DIRECTORY/*; do
+    if [ $(file -b $FILE | cut -d' ' -f1) == 'ELF' ]; then
+      if ldd $FILE | grep -q 'not found'; then
+         echo "$FILE"
+         ldd $FILE | grep 'not found' | awk '{print "Not found:", $1}'
+      fi
+    fi
+  done
+done
